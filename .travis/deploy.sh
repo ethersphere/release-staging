06324581e95cbeb5b48def2205df428757f5af90
Rafael Matias <rafael@skyle.net>
2019-01-29 12:10:01 +0100

    swarm-staging: use OnDelete strategy for updates

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,8 +2,21 @@
 
 HELMSMAN="helmsman -no-banner -no-ns -no-fancy"
 
+# Store version of current deployed statefulset
+kubectl config use-context staging
+OLD_VERSION=$(kubectl -n staging get sts swarm-staging -o jsonpath="{.metadata.resourceVersion}")
+
+# Perform updates
 for e in *.yaml
 do
   ENV="${e%.*}" # Remove the extension 'staging.yaml' becomes 'staging'
   kubectl config use-context "$ENV" && $HELMSMAN --apply -f "$ENV.yaml"
 done
+
+# Check if the statefulset was updated
+kubectl config use-context staging
+NEW_VERSION=$(kubectl -n staging get sts swarm-staging -o jsonpath="{.metadata.resourceVersion}")
+
+if [ "$OLD_VERSION" != "$NEW_VERSION" ]; then
+  kubectl -n staging get pods -l release=swarm-staging,app=swarm,component=swarm
+fi
