a8fadc1169b02534b1acd8faf451c7899f323486
Rafael Matias <rafael@skyle.net>
2019-01-30 11:03:34 +0100

    delete pods from the private cluster whenever there are changes to the StatefulSet

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,9 +2,10 @@
 
 HELMSMAN="helmsman -no-banner -no-ns -no-fancy"
 
-# Store version of current deployed statefulset
+# Store version of current deployed statefulsets
 kubectl config use-context staging
-OLD_VERSION=$(kubectl -n staging get sts swarm-staging -o jsonpath="{.metadata.resourceVersion}")
+PUBLIC_OLD_VERSION=$(kubectl -n staging get sts swarm-staging -o jsonpath="{.metadata.resourceVersion}")
+PRIVATE_OLD_VERSION=$(kubectl -n staging get sts swarm-private -o jsonpath="{.metadata.resourceVersion}")
 
 # Perform updates
 for e in *.yaml
@@ -13,10 +14,15 @@ do
   kubectl config use-context "$ENV" && $HELMSMAN --apply -f "$ENV.yaml"
 done
 
-# Check if the statefulset was updated
+# Check if the statefulsets were updated
 kubectl config use-context staging
-NEW_VERSION=$(kubectl -n staging get sts swarm-staging -o jsonpath="{.metadata.resourceVersion}")
+PUBLIC_NEW_VERSION=$(kubectl -n staging get sts swarm-staging -o jsonpath="{.metadata.resourceVersion}")
+PRIVATE_NEW_VERSION=$(kubectl -n staging get sts swarm-private -o jsonpath="{.metadata.resourceVersion}")
 
-if [ "$OLD_VERSION" != "$NEW_VERSION" ]; then
+if [ "$PUBLIC_OLD_VERSION" != "$PUBLIC_NEW_VERSION" ]; then
   kubectl -n staging delete pods -l release=swarm-staging,app=swarm,component=swarm
+fi
+
+if [ "$PRIVATE_OLD_VERSION" != "$PRIVATE_NEW_VERSION" ]; then
+  kubectl -n staging delete pods -l release=swarm-private,app=swarm-private,component=swarm
 fi
